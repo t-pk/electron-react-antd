1396ff4eb62e849d4fd6cecd7fe10608c03de13d
Tai Pham <pktai.iot@gmail.com>
2021-06-08 15:49:33 +0700

    Revert to 56e05fced

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: 'erb',
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    createDefaultProgram: true,
+  },
+  settings: {
+    'import/resolver': {
+      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
+      node: {},
+      webpack: {
+        config: require.resolve('./.erb/configs/webpack.config.eslint.js'),
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+};
